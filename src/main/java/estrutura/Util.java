4217610d01698fae53e673bc25b223acc231a7c7
eduardocamilo1985 <eduardocamilo1985@gmail.com>
2021-09-01 17:56:16 -0300

    Cria a classe Util. Metodos para Java Faker

--- src/main/java/estrutura/Util.java
+++ src/main/java/estrutura/Util.java
@@ -8,4 +8,30 @@ public class Util {
   public String email(){
    return faker.internet().emailAddress();
   }
+    public String name(){
+        return faker.funnyName().name();
+    }
+    public String lastName(){
+        return faker.name().lastName();
+    }
+    public String company(){
+        return faker.company().name();
+    }
+
+    public String address(){
+        return faker.address().streetName();
+    }
+    public String inf(){
+        return faker.chuckNorris().fact();
+    }
+    public String phone(){
+        return faker.phoneNumber().cellPhone();
+    }
+    public String city(){
+        return faker.address().city();
+    }
+    public String zipCode(){
+        return faker.number().digits(5);
+    }
+
 }
